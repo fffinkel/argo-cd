bfb52e4ac2da9e4b3c1cdce2781b05e78ca71c4e
Matt Finkel <finkel.matt@gmail.com>
2024-11-04 18:49:32 +0000

    Add config setting to preserve V2 update/delete RBAC (#20600)
    A breaking change was introduced in a previous commit that is planned to
    be a part of the next major version of Argo CD (v3) where it's okay to
    introduce breaking changes. We want this feature before we hit v3, so
    we add a config setting that allows us to explicitly turn this new v3
    behavior on in v2. The current v2 behavior is the default, so this
    change will not affect folks who do not explicitly opt in.
    
    This commit to add the gating code is added separately so it will be
    easy to either cherry pick that pervious commit or revert this one.
    
    (see also #18124, #19988)
    
    Signed-off-by: Matt Finkel <finkel.matt@gmail.com>

--- server/application/application.go
+++ server/application/application.go
@@ -1333,10 +1333,19 @@ func (s *Server) getAppResources(ctx context.Context, a *appv1.Application) (*ap
 }
 
 func (s *Server) getAppLiveResource(ctx context.Context, action string, q *application.ApplicationResourceRequest) (*appv1.ResourceNode, *rest.Config, *appv1.Application, error) {
-	if action == rbacpolicy.ActionDelete || action == rbacpolicy.ActionUpdate {
+	enableV3, err := s.settingsMgr.GetServerRBACEnableV3()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	if enableV3 && (action == rbacpolicy.ActionDelete || action == rbacpolicy.ActionUpdate) {
 		action = fmt.Sprintf("%s/%s/%s/%s/%s", action, q.GetGroup(), q.GetKind(), q.GetNamespace(), q.GetResourceName())
 	}
 	a, _, err := s.getApplicationEnforceRBACInformer(ctx, action, q.GetProject(), q.GetAppNamespace(), q.GetName())
+	if !enableV3 && err != nil && errors.Is(err, permissionDeniedErr) && (action == rbacpolicy.ActionDelete || action == rbacpolicy.ActionUpdate) {
+		action = fmt.Sprintf("%s/%s/%s/%s/%s", action, q.GetGroup(), q.GetKind(), q.GetNamespace(), q.GetResourceName())
+		a, _, err = s.getApplicationEnforceRBACInformer(ctx, action, q.GetProject(), q.GetAppNamespace(), q.GetName())
+	}
 	if err != nil {
 		return nil, nil, nil, err
 	}
